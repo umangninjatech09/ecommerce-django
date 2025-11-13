7924057654ae8b5e3cc260755b0406fd5dd452c6
Umang <umangninjatech09@gmail.com>
2025-11-13 18:13:44 +0530

    changes in user model for the product apis

--- app/products/apps.py
+++ app/products/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app.products'  # ✅ must include 'app.' prefix
+    name = 'app.products'  
     label = 'products'
--- app/users/web_views.py
+++ app/users/web_views.py
@@ -2,15 +2,29 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import RegisterForm, LoginForm, AddressForm
+from .forms import RegisterForm, LoginForm, AddressForm, RetailerRegisterForm
 from .models import Address
+from app.products.models import Product, Category
 
 
 
-@login_required
 def home_page(request):
-    return render(request, "home.html")
+    categories = Category.objects.filter(is_deleted=False)
+    category_data = []
 
+    for category in categories:
+        products = Product.objects.filter(
+            category=category, 
+            is_active=True, 
+            is_deleted=False
+        )[:6]
+
+        category_data.append({
+            "category": category,
+            "products": products
+        })
+
+    return render(request, "home.html", {"category_data": category_data})
 
 # ---------------------------
 # Register
@@ -29,6 +43,19 @@ def register_page(request):
     return render(request, "users/register.html", {"form": form})
 
 
+def retailer_register_page(request):
+    if request.method == "POST":
+        form = RetailerRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Retailer account created. Please login.")
+            return redirect("login_page")
+    else:
+        form = RetailerRegisterForm()
+    return render(request, "users/retailer_register.html", {"form": form})
+
+
+
 # ---------------------------
 # Login
 # ---------------------------
@@ -39,15 +66,22 @@ def login_page(request):
             username = form.cleaned_data["username"]
             password = form.cleaned_data["password"]
             user = authenticate(request, username=username, password=password)
-            if user is not None:
+
+            if user:
                 login(request, user)
-                messages.success(request, f"Welcome back, {user.username} 👋")
-                return redirect("home_page")
+
+                # REDIRECT BASED ON ROLE
+                if user.is_retailer or user.is_admin:
+                    return redirect("product_admin_list_page")  # Retailer dashboard
+                else:
+                    return redirect("home_page")  # Customer home page
             else:
                 messages.error(request, "Invalid username or password.")
     else:
         form = LoginForm()
+
     return render(request, "users/login.html", {"form": form})
+
 
 
 # ---------------------------
